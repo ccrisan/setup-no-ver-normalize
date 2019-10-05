773fa59752f3721b93bea8c91173e896f224482c
Calin Crisan <ccrisan@gmail.com>
2019-10-05 13:50:40 +0300

    setup.py: set long_description_content_type

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(
     name='setupnovernormalize',
-    version='1.0.0',
+    version='1.0.1',
     py_modules=['setupnovernormalize'],
     description='Prevent version normalization when packaging Python projects with setuptools.',
     long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/ccrisan/setup-no-ver-normalize',
     author='Calin Crisan',
     author_email='ccrisan@gmail.com',
